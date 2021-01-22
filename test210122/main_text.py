784a452444e1a559490a2d2ee5b3698743feac57
Constantine Shterev <kokoshterev2005@gmail.com>
2021-01-22 13:29:52 +0200

    Update of task 1

--- test210122/main_text.py
+++ test210122/main_text.py
@@ -1,6 +1,6 @@
 from search_text import search_and_replace_text
 
 text = "edno dve tri chetiri pet shest sedem osem devet deset"
-search_sentence = " osem"
+search_sentence = " osem devet"
 
 print (search_and_replace_text(search_sentence, text))
